676f12823e87afe11509807e7a709ecad2386adb
Michael Hammond <michaelduncanhammond@gmail.com>
2026-05-22 19:13:03 -0400

    Update bsod.js

--- scripts/bsod.js
+++ scripts/bsod.js
@@ -1,1 +1,8 @@
+$("#btnSave").click(function() { 
+        html2canvas($("#bsod")[0]).then((canvas) => {
 
+                canvas.toBlob(function(blob) {
+                    saveAs(blob, "BSOD.png"); 
+                });
+            });
+        });
